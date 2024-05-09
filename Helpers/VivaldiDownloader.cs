498d4d6872f67fa9cb10520973168648419f90a1
czyt <czyt@w.cn>
2024-05-09 22:18:18 +0800

    add api to get dist urls

---
+++ Helpers/VivaldiDownloader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace VivaldiUpdater.Helpers
+{
+    public static class VivaldiDownloader
+    {
+        public class VivaldiDistUrlInfo
+        {
+            public string Platform { get; set; }
+            public string Url { get; set; }
+            public string UrlMirror { get; set; }
+        }
+
+        public static async Task<VivaldiDistUrlInfo> GetVivaldiDistUrls(int timeout = 8000)
+        {
+            //try mirror first
+            var hc = new HttpClient { Timeout = TimeSpan.FromSeconds(3) };
+            try
+            {
+                return SimpleJson.SimpleJson.DeserializeObject<VivaldiDistUrlInfo>(
+                    await hc.GetStringAsync("https://vivaldi.czyt.tech/api/dist_urls"));
+            }
+            catch
+            {
+                //oops
+            }
+
+            return null;
+        }
+    }
+}
