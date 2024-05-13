25ff04f69b03ddc8a3d517a653e4705f2c2d67e8
czyt <czyt@w.cn>
2024-05-13 20:15:45 +0800

    update unit test

---
+++ TestCase/Helpers/VivaldiDownloaderTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using VivaldiUpdater.Helpers;
+
+namespace TestCase.Helpers
+{
+    [TestFixture]
+    [TestOf(typeof(VivaldiDownloader))]
+    public class VivaldiDownloaderTest
+    {
+        [Test]
+        public async Task GetVivaldiDistUrls()
+        {
+            var urls = await VivaldiDownloader.GetVivaldiDistUrls();
+            Assert.True(urls.Count(d =>
+                            !string.IsNullOrEmpty(d.Platform)
+                            && !string.IsNullOrEmpty(d.Url)
+                            && !string.IsNullOrEmpty(d.UrlMirror))
+                        >= 12);
+        }
+    }
+}
