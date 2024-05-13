44c7eed76303452d8884687eaafd53e2252798ac
czyt <czyt@w.cn>
2024-05-13 22:43:09 +0800

    add download class and unit test

---
+++ Helpers/ResumeableDownloader.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace VivaldiUpdater.Helpers
+{
+    public class ResumeableDownloader
+    {
+        public event EventHandler<DownloadProgressEventArgs> DownloadProgressChanged;
+
+        private readonly HttpClient _client;
+
+        public ResumeableDownloader()
+        {
+            _client = new HttpClient();
+        }
+
+
+        public async Task DownloadFileAsync(string requestUri, string outputPath)
+        {
+            try
+            {
+                // 请求服务器文件大小
+                var response = await _client.GetAsync(requestUri, HttpCompletionOption.ResponseHeadersRead);
+                response.EnsureSuccessStatusCode();
+
+                var contentLength = response.Content.Headers.ContentLength ?? 0;
+
+                var rangeRequest = new HttpRequestMessage(HttpMethod.Get, requestUri);
+                var currentPosition = 0L;
+
+                if (File.Exists(outputPath))
+                {
+                    var fileInfo = new FileInfo(outputPath);
+                    currentPosition = fileInfo.Length;
+
+                    if (currentPosition == contentLength)
+                    {
+                        // 文件已经下载完毕
+                        OnDownloadProgressChanged(new DownloadProgressEventArgs(100, currentPosition));
+                        return;
+                    }
+
+                    rangeRequest.Headers.Add("Range", $"bytes={currentPosition}-");
+                }
+
+                using (var rangeResponse =
+                       await _client.SendAsync(rangeRequest, HttpCompletionOption.ResponseHeadersRead))
+                {
+                    rangeResponse.EnsureSuccessStatusCode();
+
+                    using (var fileStream = new FileStream(outputPath, FileMode.Append, FileAccess.Write))
+                    using (var rangeStream = await rangeResponse.Content.ReadAsStreamAsync())
+                    {
+                        var buffer = new byte[8192];
+                        var bytesRead = 0;
+
+                        while ((bytesRead = await rangeStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                        {
+                            await fileStream.WriteAsync(buffer, 0, bytesRead);
+                            currentPosition += bytesRead;
+
+                            var progressPercentage = (int)(((double)currentPosition / contentLength) * 100);
+                            OnDownloadProgressChanged(
+                                new DownloadProgressEventArgs(progressPercentage, currentPosition));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+
+        protected virtual void OnDownloadProgressChanged(DownloadProgressEventArgs e)
+        {
+            DownloadProgressChanged?.Invoke(this, e);
+        }
+    }
+
+    public class DownloadProgressEventArgs : EventArgs
+    {
+        public int ProgressPercentage { get; private set; }
+        public long BytesReceived { get; private set; }
+
+        public DownloadProgressEventArgs(int progressPercentage, long bytesReceived)
+        {
+            ProgressPercentage = progressPercentage;
+            BytesReceived = bytesReceived;
+        }
+    }
+}
---
+++ TestCase/Helpers/ResumeableDownloaderTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using VivaldiUpdater.Helpers;
+
+namespace TestCase.Helpers
+{
+    [TestFixture]
+    [TestOf(typeof(ResumeableDownloader))]
+    public class ResumeableDownloaderTest
+    {
+        private ResumeableDownloader downloader;
+
+        private EventHandler<DownloadProgressEventArgs> DownloadChanged = (sender, e) =>
+        {
+            TestContext.WriteLine($"Download progress: {e.ProgressPercentage}% Bytes received: {e.BytesReceived}");
+        };
+
+
+        [SetUp]
+        public void Setup()
+        {
+            downloader = new ResumeableDownloader();
+            downloader.DownloadProgressChanged += DownloadChanged;
+        }
+
+
+        [TearDown]
+        public void Cleanup()
+        {
+            downloader.DownloadProgressChanged -= DownloadChanged;
+        }
+
+        [Test]
+        public async Task TestDownloadFile()
+        {
+            string url = "https://hole.czyt.tech/magic/https/downloads.vivaldi.com/stable/Vivaldi.6.7.3329.27.x64.exe";
+            string outputPath = "Vivaldi.6.7.3329.27.x64.exe";
+
+            await downloader.DownloadFileAsync(url, outputPath);
+
+            if (System.IO.File.Exists(outputPath))
+            {
+                System.IO.File.Delete(outputPath);
+            }
+
+            Assert.Pass();
+        }
+    }
+}
