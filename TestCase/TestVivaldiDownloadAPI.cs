b38c28fb8ab2e0ac164bf5508f299be773f85076
虫子樱桃 <czyt.go@gmail.com>
2024-05-13 15:04:55 +0800

    Add unit Test for test functions

---
+++ TestCase/TestVivaldiDownloadAPI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using VivaldiUpdater.Helpers;
+
+namespace Test
+{
+    [TestFixture]
+    public class TestVivaldiDownloadAPI
+    {
+        [Test]
+        public async Task GetAllDownloadUrls()
+        {
+           var urls = await VivaldiDownloader.GetVivaldiDistUrls();
+           Console.WriteLine(urls);
+        }
+        
+    }
+}
